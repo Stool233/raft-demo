d9135df767345b578b809f2dce1af5aeb41f33df
stool <christry.stool@gmail.com>
2019-05-19 23:00:28 +0800

    Initial commit

---
+++ src/main/java/org/stool/raftdemo/RaftDemoApplication.java
@@ -0,0 +1,13 @@
+package org.stool.raftdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaftDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RaftDemoApplication.class, args);
+    }
+
+}
